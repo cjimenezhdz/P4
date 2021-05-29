29b3873bb3c2f8bf7e34f90e354826ad764363fd
Percy Wilberth Bonett Mendoza <percy96bm@gmail.com>
2021-05-29 11:57:13 +0200

    actualización nueva 2

--- scripts/run_spkid.sh
+++ scripts/run_spkid.sh
@@ -99,7 +99,7 @@ compute_lp() {
 compute_lpcc() {
     for filename in $(cat $lists/class/all.train $lists/class/all.test); do
         mkdir -p `dirname $w/$FEAT/$filename.$FEAT`
-        EXEC="wav2lpcc 38 25 $db/$filename.wav $w/$FEAT/$filename.$FEAT"
+        EXEC="wav2lpcc 50 35 $db/$filename.wav $w/$FEAT/$filename.$FEAT"
         echo $EXEC && $EXEC || exit 1
     done
 }
